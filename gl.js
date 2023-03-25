30c3564d65acdae2443aace5fff7ff6e5092f2e5
q6rccn <q6rccn@gmail.com>
2023-03-25 08:36:40 -0700

    fixed typo

---
+++ gl.js
@@ -0,0 +1,14 @@
+function main() {
+	const canvas = document.querySelector('#glcanvas')
+	
+	const gl = canvas.getContext("webgl")
+	if (gl == null) {
+		alert('cannot initialize webgl')
+		return
+	}
+	
+	serial.log("hello")
+	
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	gl.clear(gl.COLOR_BUFFER_BIT)
+}
