32807f2d28c7c648898b359f552051f248353b06
semele <gavratcliff@gmail.com>
2024-01-23 12:49:21 -0500

    update

--- generate_posts_index.py
+++ generate_posts_index.py
@@ -26,15 +26,27 @@ outro = """
 
 str = "" + intro
 
+blog_list = ""
+
 for file in os.listdir('posts'):
     if file.endswith('.html'):
         name = (os.path.splitext(file))[0].replace('_', ' ')
         date = datetime.fromtimestamp(
-                os.path.getctime("posts\\" + file)).strftime('%Y-%m-%d')
+                os.path.getctime("posts/" + file)).strftime('%Y-%m-%d')
         str += "<li><a href=\"posts/" + file + "\">" + name + "</a> - " + date + "</li>"
         print(name)
+        blog_list += "<li><a href=\"posts/" + file + "\">" + name + "</a> - " + date + "</li>"
 
 str += outro
 
 out_file = open('posts.html', 'w')
 out_file.write(str)
+
+
+in_file = open('index_template.html', 'r')
+template = in_file.read()
+
+template = template.replace("<<<blogs>>>", blog_list)
+
+out_file = open('index.html', 'w')
+out_file.write(template)
--- gl.js
+++ gl.js
@@ -1,14 +1,16 @@
 function main() {
-	const canvas = document.querySelector('#glcanvas')
+	const canvas = document.getElementById("glcanvas");
 	
-	const gl = canvas.getContext("webgl")
+	const gl = canvas.getContext("webgl");
 	if (gl == null) {
-		alert('cannot initialize webgl')
-		return
+		alert('cannot initialize webgl');
+		return;
 	}
 	
-	serial.log("hello")
-	
-	gl.clearColor(0.0, 0.0, 0.0, 1.0)
-	gl.clear(gl.COLOR_BUFFER_BIT)
+	const indexBuffer = initIndexBuffer(gl);
+
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
+//main();
